345d1ce2492a58927dbafcf12ee15f252e7ad66f
MAkbarD <mad4869@gmail.com>
2023-04-12 21:27:01 +0700

    Cleaning up main.py

--- main.py
+++ main.py
@@ -14,13 +14,13 @@
     # >>> Dimasukkan ke dalam LIST sebagai PARAM dari FUNCTION
     # add_item([<nama item>, <jumlah item>, <harga per item>])
 from inputs import input_item
-import add_item
+from add_item import add_item
 
 nama_items, jumlah_items, harga_items = input_item()
 
 order_items = add_item(nama_items, jumlah_items, harga_items)
 
-# 3. Customer dapat melakukan UPDATE DATA
+# 2. Customer dapat melakukan UPDATE DATA
     # >>> UPDATE NAMA ITEM dengan FUNCTION update_nama_item(<nama item>, <update nama>)
     # >>> UPDATE JUMLAH ITEM dengan FUNCTION update_jumlah_item(<nama item>, <update jumlah>)
     # >>> UPDATE HARGA ITEM dengan FUNCTION update_harga_item(<nama item>, <update harga>)
@@ -42,7 +42,7 @@ from update_item import update_item_price
 nama_item, harga_item = input_update_price(order_items)
 update_item_price(nama_item, harga_item, order_items)
 
-# 4. Customer dapat MEMBATALKAN TRANSAKSI
+# 3. Customer dapat MEMBATALKAN TRANSAKSI
     # >>> MENGHAPUS ITEM dengan FUNCTION delete_item(<nama_item>)
     # >>> MENGHAPUS SEMUA TRANSAKSI dengan FUNCTION reset_transaction()
 from inputs import input_delete
@@ -50,21 +50,21 @@ from delete_item import delete_item
 
 nama_item = input_delete(order_items)
 
-delete_item(nama_item)
+delete_item(nama_item, order_items)
 
 from delete_item import reset_transaction
 
-reset_transaction()
+reset_transaction(order_items)
 
-# 5. Customer dapat MEMERIKSA ULANG PESANAN
+# 4. Customer dapat MEMERIKSA ULANG PESANAN
     # >>> Menggunakan FUNCTION check_order()
         # >>> Mengeluarkan pesan PEMESANAN SUDAH BENAR apabila TIDAK ADA KESALAHAN INPUT
         # >>> Mengeluarkan pesan TERDAPAT KESALAHAN INPUT apabila ADA KESALAHAN INPUT
             # >>> Mengeluarkan OUTPUT TRANSAKSI
                 # >>> Terdiri dari NOMOR, NAMA ITEM, JUMLAH ITEM, HARGA/ITEM, TOTAL HARGA
-import check_order
+from check_order import check_order
 
-table_items = check_order()
+table_items = check_order(order_items)
 
 # 6. Customer dapat MENGHITUNG TOTAL BELANJA
     # >>> Menggunakan FUNCTION check_out()
@@ -81,6 +81,6 @@ table_items = check_order()
             # v. total_harga
             # vi. diskon
             # vii. harga_diskon
-import check_out
+from check_out import check_out
 
-check_out()
+check_out(table_items)
